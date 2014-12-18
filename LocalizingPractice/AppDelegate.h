ec06de8fc6c8100f6a4a292478598e4d120bd885
woonohyo <woonohyo@nhnnext.org>
2014-12-18 11:44:58 +0900

    Initial Commit

---
+++ LocalizingPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalizingPractice
+//
+//  Created by Wonhyo Yi on 2014. 12. 18..
+//  Copyright (c) 2014년 Woonohyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalizingPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalizingPractice
+//
+//  Created by Wonhyo Yi on 2014. 12. 18..
+//  Copyright (c) 2014년 Woonohyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
